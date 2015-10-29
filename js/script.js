58e0728e22442001ac3ec51ac91523e68e37063a
Christian Marienfeld <post@chrisland.de>
2015-10-29 14:45:27 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+var _tasks = {
+  'home': function () {
+    console.log('home');
+  },
+  'show': function () {
+    console.log('show');
+  }
+};
+
+var _app = {
+  init: function () {
+
+    this.eventListener();
+    this.renderPage('home', $('#nav .pageBtn.start'));
+
+  },
+  eventListener: function () {
+
+    $('.pageBtn').on('click', function (e) {
+      var page = $(this).data('page');
+      if (page) {
+        _app.renderPage(page, this);
+      }
+    });
+
+  },
+  eventTask: function(page) {
+
+    if (typeof _tasks[page] === 'function') {
+      _tasks[page]();
+    }
+
+  },
+  renderPage: function (page, btn) {
+
+    $('#page').load('tmpl/'+page+'.html', function () {
+      _app.setNavActive(btn);
+      _app.eventTask(page);
+      _app.eventListener();
+    });
+
+
+  },
+  setNavActive: function (btn) {
+
+    $('#nav').find('.pageBtn.active').removeClass('active');
+    $(btn).addClass('active');
+
+  }
+
+};
+
+
+$( document ).ready(function() {
+
+  _app.init();
+
+});
